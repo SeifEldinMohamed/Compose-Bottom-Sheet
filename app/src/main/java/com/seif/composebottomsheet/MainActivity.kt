967cb8cd732dd94ffdd84412aab85693144f0f47
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-10-11 22:00:38 +0200

    Bottom Sheet Added

--- app/src/main/java/com/seif/composebottomsheet/MainActivity.kt
+++ app/src/main/java/com/seif/composebottomsheet/MainActivity.kt
@@ -3,26 +3,71 @@ package com.seif.composebottomsheet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.seif.composebottomsheet.ui.theme.ComposeBottomSheetTheme
+import kotlinx.coroutines.launch
 
+@ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBottomSheetTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val sheetState = rememberBottomSheetState( // for handle expanded and collapse states
+                    initialValue = BottomSheetValue.Collapsed
+                )
+                val scaffoldState = rememberBottomSheetScaffoldState(
+                    bottomSheetState = sheetState
+                )
+                val scope = rememberCoroutineScope()
+                BottomSheetScaffold( // a wrapper around our sheet that deals with toggling our bottom sheet and making sure we can add content to it
+                    scaffoldState = scaffoldState,
+                    sheetContent = {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(300.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = "Bottom sheet",
+                                fontSize = 60.sp
+                            )
+                        }
+                    },
+                    sheetBackgroundColor = Color.Green,
+                    sheetPeekHeight = 0.dp
                 ) {
-                    Greeting("Android")
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Button(onClick = {
+                            scope.launch {
+                                if(sheetState.isCollapsed) {
+                                    sheetState.expand()
+                                } else {
+                                    sheetState.collapse()
+                                }
+                            }
+                        }) {
+                            Text(text = "Bottom sheet fraction: ${sheetState.progress.fraction}")
+                            // this fraction value is important if we want to link an animation to the bottom sheet
+                        }
+                    }
                 }
             }
         }
